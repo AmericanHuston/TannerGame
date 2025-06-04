67002573a1c980dc517430bf2c89c02f1aed6292
AmericanHuston <americanhuston@gmail.com>
2025-06-04 15:40:20 -0600

    Fixing a whoopsie

--- install/install.bat
+++ install/install.bat
@@ -1,15 +1,5 @@
 @echo off
 
-:checkPython
-if exist "%LOCALAPPDATA%\Programs\Python\Python313\python.exe" (
-  echo Python is installed.
-  goto SKIP_PY
-) else (
-  echo Python is not installed, installing now
-  winget install -e --id Python.Python.3.13
-)
-EXIT /B 0
-
 if exist "./TannerGame/game.py" (
   goto RUNGAME
 )
@@ -21,16 +11,6 @@ if %errorlevel% == 0 (
 ) else (
   echo Git is not installed or not in PATH, installing now
   winget install --id Git.Git -e --source winget
-  if exist "%LOCALAPPDATA%\Programs\Python\Python313\python.exe" (
-    echo Python is installed, closing this window.
-    echo Run this script again to finish the install.
-  ) else (
-    echo Python is not installed, installing now
-    winget install -e --id Python.Python.3.13
-  )
-  REM git.exe clone https://github.com/AmericanHuston/TannerGame.git
-)
-
   git.exe clone https://github.com/AmericanHuston/TannerGame.git
 )
 
